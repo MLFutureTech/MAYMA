fda6a93133311018d2b42215a1915aea97606f30
Yoghtenhaf <joghtenhaf@localhost>
2023-10-17 21:29:07 -0600

    Se prueba ajuste de hijuelos a tamaño medio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ const Home: React.FC = () => {
         <IonGrid fixed>
           <Section imagen={''} odd={true}>
             <IonRow>
-              <IonCol size="2" sizeMd='0'></IonCol>
+              <IonCol sizeXl="2" sizeMd='0'></IonCol>
               <IonCol sizeXl="4" sizeMd='6' sizeSm='6'>
                 <Hijuelo titulo="Hijuelo chico" altura="entre 30 y 45 cm" cantidad="35,000 pzs" precio="$13.0 MXN" >
                   <IonImg src="MAYMA/assets/images/SAVE_20220427_221219.jpg" />
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
                   <IonImg src="MAYMA/assets/images/SAVE_20220427_221219.jpg" />
                 </Hijuelo>
               </IonCol>
-              <IonCol size="2" sizeMd='0'></IonCol>
+              <IonCol sizeXl="2" sizeMd='0'></IonCol>
             </IonRow>
           </Section>
         </IonGrid>
